175fd9e6fa7cfcc60b8a2e983fae09d25d1497af
luankleber <luankleber.sp@outlook.com>
2022-01-11 18:19:20 -0300

    taking the data from each pokemon

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,23 @@ function App() {
   const [allPokemons, setAllPokemons] = useState([])
   const [loadMore, setLoadMore] = useState('http://pokeapi.co/api/v2/pokemon?limit=20')
 
-  const getAllPokemons = async() => {
+  const getAllPokemons = async () => {
     const res = await fetch(loadMore)
     const data = await res.json()
 
-    console.log(data)
+    setLoadMore(data.next)
+
+    function createPokemonObject (result) {
+      result.forEach( async (pokemon) => {
+        const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`)
+        const data =  await res.json()
+
+        setAllPokemons( currentList => [...currentList, data])
+
+      })
+    }
+    createPokemonObject(data.results)
+    await console.log(allPokemons)
   }
 
   useEffect(() =>{
