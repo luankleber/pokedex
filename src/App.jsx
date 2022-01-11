bfa4cc3dc1e640c11309a8de61c5de7af8812a74
luankleber <luankleber.sp@outlook.com>
2022-01-11 17:23:33 -0300

    adding the 20 first pokemon

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,30 @@
+import { useEffect, useState } from "react"
 
 function App() {
+
+  const [allPokemons, setAllPokemons] = useState([])
+  const [loadMore, setLoadMore] = useState('http://pokeapi.co/api/v2/pokemon?limit=20')
+
+  const getAllPokemons = async() => {
+    const res = await fetch(loadMore)
+    const data = await res.json()
+
+    console.log(data)
+  }
+
+  useEffect(() =>{
+    getAllPokemons()
+  }, [])
+
   return (
     <div className="app-container">
       <h1>PokeDEX</h1>
+      <div className="pokemon-container">
+        <div className="all-container">
+
+        </div>
+        <button className="load-more">Load More</button>
+      </div>
     </div>
   );
 }
