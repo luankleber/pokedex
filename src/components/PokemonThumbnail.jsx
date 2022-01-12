cebf562c8aecc4ef668734238b0ff4df2e8b5374
luankleber <luankleber.sp@outlook.com>
2022-01-11 21:08:50 -0300

    pre fix on thumbs

--- src/components/PokemonThumbnail.jsx
+++ src/components/PokemonThumbnail.jsx
@@ -7,7 +7,7 @@ const PokemonThumbnail = ({id, name, image, type}) => {
             <div className='number'>
                 <small>#0{id}</small>
             </div>
-            <img src={image} alt={name} />
+            <img src={image} alt={name}/>
             <div className='detail-wrapper'>
                 <h3>{name}</h3>
                 <small>Type: {type}</small>
