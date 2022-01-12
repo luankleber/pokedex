dc277f4112a46128183f83cb48ca23e66004c08e
luankleber <luankleber.sp@outlook.com>
2022-01-12 00:49:27 -0300

    fix the texts and add type colors

--- src/components/PokemonThumbnail.jsx
+++ src/components/PokemonThumbnail.jsx
@@ -2,16 +2,18 @@ import React from 'react';
 
 const PokemonThumbnail = ({id, name, image, type}) => {
     const style = `thumb-container ${type}`
+    const type_style = `poke-type type_${type}`  
+
     return(
         <div className={style}>
-            <div className='number'>
-                <small>#0{id}</small>
-            </div>
             <img src={image} alt={name}/>
+            <div>
+                <small className='poke-number'>#0{id}</small>
+            </div>
             <div className='detail-wrapper'>
                 <h3>{name}</h3>
-                <small>Type: {type}</small>
             </div>
+                <small className={type_style}>{type}</small>
         </div>
     )
 }
